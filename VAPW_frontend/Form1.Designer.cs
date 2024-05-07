4aa715afe4e9130400c5d3f30efd3e40f419e61b
konerzajakub <jakubkonera@email.cz>
2024-05-07 14:04:07 +0200

    added buttons, progress bar, scaling of pictures

--- VAPW_frontend/Form1.Designer.cs
+++ VAPW_frontend/Form1.Designer.cs
@@ -28,89 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
             pictureBox6 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            progressBar1 = new ProgressBar();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.car;
-            pictureBox1.Location = new Point(12, 243);
+            pictureBox1.Location = new Point(12, 478);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1009, 334);
+            pictureBox1.Size = new Size(254, 154);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            pictureBox2.Location = new Point(898, 294);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(43, 283);
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
-            pictureBox2.Click += pictureBox2_Click;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.ErrorImage = Properties.Resources.semafor;
-            pictureBox3.Location = new Point(1062, 348);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(42, 150);
-            pictureBox3.TabIndex = 2;
-            pictureBox3.TabStop = false;
-            // 
             // pictureBox4
             // 
-            pictureBox4.ErrorImage = Properties.Resources.baracek;
-            pictureBox4.Location = new Point(319, 115);
+            pictureBox4.ErrorImage = (Image)resources.GetObject("pictureBox4.ErrorImage");
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.InitialImage = (Image)resources.GetObject("pictureBox4.InitialImage");
+            pictureBox4.Location = new Point(508, 86);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(561, 451);
+            pictureBox4.Size = new Size(424, 568);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 3;
             pictureBox4.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(478, 439);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(45, 206);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(906, 423);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(46, 209);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(367, 302);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(126, 120);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
             // pictureBox6
             // 
-            pictureBox6.Location = new Point(486, 149);
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(984, 288);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(95, 320);
-            pictureBox6.TabIndex = 5;
+            pictureBox6.Size = new Size(144, 112);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 7;
             pictureBox6.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(117, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(122, 59);
+            button1.TabIndex = 8;
+            button1.Text = "jet autem";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(117, 177);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 57);
+            button2.TabIndex = 9;
+            button2.Text = "pripravit mycku";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(677, 28);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(255, 36);
+            progressBar1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(508, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 30);
+            label1.TabIndex = 11;
+            label1.Text = "progress mytí";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1258, 666);
+            Controls.Add(label1);
+            Controls.Add(progressBar1);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(pictureBox6);
-            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox5);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
+            Controls.Add(pictureBox4);
             Controls.Add(pictureBox1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
         private PictureBox pictureBox6;
+        private Button button1;
+        private Button button2;
+        private ProgressBar progressBar1;
+        private Label label1;
     }
 }
--- VAPW_frontend/Form1.cs
+++ VAPW_frontend/Form1.cs
@@ -16,5 +16,10 @@ namespace VAPW_frontend
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
